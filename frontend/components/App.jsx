ac6d6777cbe9317bd10bc042cb9171312c1564f5
Samtfm <sfmemail@gmail.com>
2017-05-15 17:28:51 -0700

    create bench index

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,14 +3,15 @@ import GreetingContainer from './greeting_container';
 import { Route } from 'react-router-dom';
 import SessionFormContainer from './session_form_container';
 import { AuthRoute } from '../util/route_util';
+import BenchIndexContainer from './bench_index_container';
 
 const App = () => (
   <div>
     <h1>Bench BnB</h1>
     <GreetingContainer />
     <AuthRoute path='/signup' component={SessionFormContainer} />
-      <AuthRoute path='/login' component={SessionFormContainer} />
-
+    <AuthRoute path='/login' component={SessionFormContainer} />
+    <Route exact path='/' component={BenchIndexContainer} />
 </div>
 );
 
