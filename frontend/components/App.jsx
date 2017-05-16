2ae3aae1a0aec59a2fe4fde30bf6c6d243d94e13
Samtfm <sfmemail@gmail.com>
2017-05-15 17:44:33 -0700

    add map and search components

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,7 +3,7 @@ import GreetingContainer from './greeting_container';
 import { Route } from 'react-router-dom';
 import SessionFormContainer from './session_form_container';
 import { AuthRoute } from '../util/route_util';
-import BenchIndexContainer from './bench_index_container';
+import SearchContainer from './search_container';
 
 const App = () => (
   <div>
@@ -11,7 +11,7 @@ const App = () => (
     <GreetingContainer />
     <AuthRoute path='/signup' component={SessionFormContainer} />
     <AuthRoute path='/login' component={SessionFormContainer} />
-    <Route exact path='/' component={BenchIndexContainer} />
+    <Route exact path='/' component={SearchContainer} />
 </div>
 );
 
